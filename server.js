9deb6c092fefee9dd30c21aaabed588c5d11c240
mdsufiyanansari <mdsufiyanansari812002@gmail.com>
2026-08-15 22:00:17 +0530

    refactor: comment out health check log and update server listen address

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
