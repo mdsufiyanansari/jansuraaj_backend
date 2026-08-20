a14e1b72d88ec9d0bd5d4f8df3ba3a83b101bdcb
mdsufiyanansari <mdsufiyanansari812002@gmail.com>
2026-08-20 21:34:51 +0530

    feat: update health check message with emoji

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get("/health", (req, res) => {
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Jansuraaj API is running",
+    message: "🚀Jansuraaj API is running",
   });
 });
 
